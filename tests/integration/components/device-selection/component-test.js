ce63d8f022e015630c30e3b5c137cc1bccc0b724
Xander Dumaine <xander.dumaine@inin.com>
2016-03-13 12:52:39 -0400

    yay tests

--- tests/integration/components/device-selection/component-test.js
+++ tests/integration/components/device-selection/component-test.js
@@ -6,8 +6,13 @@ const tHelper = Ember.Helper.extend({
     compute: (params) => params[0]
 });
 
+const andHelper = Ember.Helper.extend({
+    compute: (params) => params[0] && params[1]
+});
+
 const webrtcService = Ember.Service.extend({
     canShareAudio: true,
+    canListDevices: true,
     canShareVideo: true,
     cameraList: Ember.A(),
     microphoneList: Ember.A(),
@@ -15,21 +20,76 @@ const webrtcService = Ember.Service.extend({
     resolutionList: Ember.A()
 });
 
-moduleForComponent('device-selection', 'Integration | Component | device selection', {
-    integration: true,
-    beforeEach: function () {
-        this.register('helper:t', tHelper);
-        this.register('service:webrtc', webrtcService);
-    }
-});
+const mockDevices = [
+    { deviceId: 'foobar', label: 'foobar' },
+    { deviceId: 'fizzbuzz', label: 'fizzbuzz' }
+];
 
-test('it renders', function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });"
-
+const renderDefault = function () {
     this.render(hbs`
         {{device-selection}}
     `);
+};
+
+moduleForComponent('device-selection', 'Integration | Component | device selection', {
+    integration: true,
+
+    beforeEach: function () {
+        this.register('helper:t', tHelper);
+        this.register('helper:and', andHelper);
+        this.register('service:webrtc', webrtcService);
+        this.inject.service('webrtc', { as: 'webrtc' });
+        this.renderDefault = renderDefault.bind(this);
+    }
+});
+
+test('it renders', function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });"
+
+    this.renderDefault();
 
     assert.equal(this.$('.options').length, 1);
 });
+
+test('it shows a message iff the browser cannot enumerate devices', function (assert) {
+    this.set('webrtc.canListDevices', false);
+    this.renderDefault();
+    assert.equal(this.$('.browser-settings-message').length, 1);
+
+    this.set('webrtc.canListDevices', true);
+    this.renderDefault();
+    assert.equal(this.$('.browser-settings-message').length, 0);
+});
+
+test('it shows a camera select and resolution select iff there are cameras, and video is true', function (assert) {
+    this.set('webrtc.cameraList', mockDevices);
+    this.renderDefault();
+    assert.equal(this.$('select').length, 2);
+
+    this.set('webrtc.cameraList', []);
+    this.renderDefault();
+    assert.equal(this.$('select').length, 0);
+
+    this.set('webrtc.cameraList', mockDevices);
+    this.render(hbs`
+        {{device-selection video=false}}
+    `);
+    assert.equal(this.$('select').length, 0);
+});
+
+test('it shows microphone select iff there are microphones and audio is true', function (assert) {
+    this.set('webrtc.microphoneList', mockDevices);
+    this.renderDefault();
+    assert.equal(this.$('select').length, 1);
+
+    this.set('webrtc.microphoneList', mockDevices);
+    this.render(hbs`
+        {{device-selection audio=false}}
+    `);
+    assert.equal(this.$('select').length, 0);
+});
+
+//TODO: test initial selection of the selects
+
+//TODO: test changing value of the selects
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import resolver from './helpers/resolver';
+import registerSelectHelper from './helpers/register-select-helper';
+registerSelectHelper();
 import {
-  setResolver
+    setResolver
 } from 'ember-qunit';
 
 setResolver(resolver);
